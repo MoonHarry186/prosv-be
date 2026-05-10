719f1a8f8e6c373d36483f56205637c2ab81cba0
Harry Moon <harrymoon1862@gmail.com>
2026-05-10 20:15:06 +0700

    feat(server): support firebase service account via env variable

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,14 +2,24 @@ import admin from 'firebase-admin';
 import path from 'path';
 import logger from '../shared/logger';
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const serviceAccount = require(path.resolve(__dirname, '../../firebase-service-account.json'));
+let serviceAccount;
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-  logger.info('Firebase Admin initialized');
+try {
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    serviceAccount = require(path.resolve(__dirname, '../../firebase-service-account.json'));
+  }
+
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+    logger.info('Firebase Admin initialized');
+  }
+} catch (error) {
+  logger.error('Failed to initialize Firebase Admin', error);
 }
 
 export default admin;
